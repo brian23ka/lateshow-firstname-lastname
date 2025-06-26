a1397d24e1aea0c4a65a240a0ac15e6a9879e2c7
Brian Kaloki <brianngenye@gmail.com>
2025-06-26 16:17:36 +0300

    final review

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_restful import Api, Resource
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -13,7 +14,49 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
 
-    from .routes import api_bp
-    app.register_blueprint(api_bp)
+    api = Api(app)
+
+    from .models import Episode, Guest, Appearance
+
+    class EpisodesResource(Resource):
+        def get(self):
+            return [e.to_dict() for e in Episode.query.all()]
+
+    class EpisodeResource(Resource):
+        def get(self, id):
+            episode = Episode.query.get(id)
+            if not episode:
+                return {"error": "Episode not found"}, 404
+            data = episode.to_dict()
+            data["appearances"] = [a.to_dict() for a in episode.appearances]
+            return data
+
+    class GuestsResource(Resource):
+        def get(self):
+            return [g.to_dict() for g in Guest.query.all()]
+
+    class AppearancesResource(Resource):
+        def post(self):
+            data = request.get_json()
+            try:
+                rating = data["rating"]
+                guest_id = data["guest_id"]
+                episode_id = data["episode_id"]
+            except (KeyError, TypeError):
+                return {"errors": ["Missing data"]}, 400
+
+            appearance = Appearance(rating=rating, guest_id=guest_id, episode_id=episode_id)
+            errors = appearance.validate()
+            if errors:
+                return {"errors": errors}, 400
+
+            db.session.add(appearance)
+            db.session.commit()
+            return appearance.to_dict(), 201
+
+    api.add_resource(EpisodesResource, "/episodes")
+    api.add_resource(EpisodeResource, "/episodes/<int:id>")
+    api.add_resource(GuestsResource, "/guests")
+    api.add_resource(AppearancesResource, "/appearances")
 
     return app
